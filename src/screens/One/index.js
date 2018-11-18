893d615c4862e0a931d5e4b2b52a6d048f83059e
Pavel Vashkel <p.vashkel@datarockets.com>
2018-11-18 19:54:41 +0300

    Add SwipableItem to the One screen along with a button for enabling/disabling side menu

--- src/screens/One/index.js
+++ src/screens/One/index.js
@@ -1,16 +1,51 @@
-import React, { PureComponent } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { Navigation } from 'react-native-navigation'
 
-class One extends PureComponent {
+import SwipableItem from 'src/components/SwipableItem'
+
+import styles from './styles'
+
+class One extends Component {
+  state = {
+    isSideMenuEnabled: true,
+  }
+
   render() {
     return (
       <View>
-        <Text>
-          Screen One
-        </Text>
+        <TouchableOpacity
+          style={styles.container}
+          onPress={this.onToggleSideMenuPress}
+        >
+          <Text>
+            {this.toggleButtonText()}
+          </Text>
+        </TouchableOpacity>
+        <SwipableItem/>
       </View>
     )
   }
+
+  onToggleSideMenuPress = () =>
+    this.setState(
+      (state) => ({ isSideMenuEnabled: !state.isSideMenuEnabled }),
+      () => this.toggleSideMenuEnabled(this.state.isSideMenuEnabled)
+    )
+
+  toggleSideMenuEnabled = enabled =>
+    Navigation.mergeOptions(this.props.componentId, {
+      sideMenu: {
+        left: {
+          enabled: enabled,
+        },
+      },
+    })
+
+  toggleButtonText = () =>
+    this.state.isSideMenuEnabled
+      ? 'Disable Side Menu'
+      : 'Enable Side Menu'
 }
 
 export default One
---
+++ src/screens/One/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    borderColor: '#EEEEEE',
+    borderWidth: 1,
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+})

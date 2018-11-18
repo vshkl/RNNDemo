cccdfd22cd55f590a076372ccb3b22b364ae0f81
Pavel Vashkel <p.vashkel@datarockets.com>
2018-11-18 18:54:09 +0300

    Add reset root action to the side menu

--- src/screens/SideMenu/index.js
+++ src/screens/SideMenu/index.js
@@ -1,16 +1,29 @@
 import React, { PureComponent } from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import { Navigation } from 'react-native-navigation'
+
+import navigation from 'src/navigation'
+
+import styles from './styles'
 
 class SideMenu extends PureComponent {
   render() {
     return (
-      <View>
-        <Text>
-          Screen Side Menu
-        </Text>
-      </View>
+      <SafeAreaView>
+        <TouchableOpacity
+          style={styles.containerResetButton}
+          onPress={this.resetRoot}
+        >
+          <Text style={styles.textResetButton}>
+            Reset Root
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     )
   }
+
+  resetRoot = () =>
+    Navigation.setRoot(navigation.main())
 }
 
 export default SideMenu
---
+++ src/screens/SideMenu/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  containerResetButton: {
+    backgroundColor: '#FAFAFA',
+    borderColor: '#EEEEEE',
+    borderWidth: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  textResetButton: {
+    fontSize: 16,
+    fontWeight: '600',
+    paddingStart: 16,
+  },
+})

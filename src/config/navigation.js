5f6b450a2e55af3748b078834b621d0ab5fcb1ff
Pavel Vashkel <p.vashkel@datarockets.com>
2018-11-18 18:26:23 +0300

    Add logic for registering screens

---
+++ src/config/navigation.js
@@ -0,0 +1,19 @@
+import { Navigation } from 'react-native-navigation'
+
+import screens from '../screens'
+
+const components = [
+  { name: 'screen.Four', component: () => screens.Four },
+  { name: 'screen.One', component: () => screens.One },
+  { name: 'screen.SideMenu', component: () => screens.SideMenu },
+  { name: 'screen.Three', component: () => screens.Three },
+  { name: 'screen.Two', component: () => screens.Two },
+]
+
+const registerScreens = () => {
+  components.forEach(component => Navigation.registerComponent(component.name, component.component))
+}
+
+export default {
+  registerScreens,
+}

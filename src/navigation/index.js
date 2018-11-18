7662cb10da277f96f20587faa336f6ed2414234e
Pavel Vashkel <p.vashkel@datarockets.com>
2018-11-18 18:45:41 +0300

    Add third and fourth tabs

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -36,12 +36,50 @@ const two = () => ({
   },
 })
 
+const three = () => ({
+  stack: {
+    id: 'tab.Three',
+    children: [
+      {
+        component: {
+          id: 'screen.Three',
+          name: 'screen.Three',
+          options: {
+            bottomTab: {
+              text: 'Three',
+            },
+          },
+        },
+      }
+    ],
+  },
+})
+
+const four = () => ({
+  stack: {
+    id: 'tab.Four',
+    children: [
+      {
+        component: {
+          id: 'screen.Four',
+          name: 'screen.Four',
+          options: {
+            bottomTab: {
+              text: 'Four',
+            },
+          },
+        },
+      }
+    ],
+  },
+})
+
 const main = () => ({
   root: {
     sideMenu: {
       left: {
         component: {
-          id: 'navigation.SideMenuScreen',
+          id: 'navigation.SideMenu',
           name: 'screen.SideMenu',
         },
       },
@@ -51,6 +89,8 @@ const main = () => ({
           children: [
             one(),
             two(),
+            three(),
+            four(),
           ],
         },
       },
